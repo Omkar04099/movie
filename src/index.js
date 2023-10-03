4c7f5f42992ab2f5e9f4bdf5466a62f7cb3b5a25
Waikar <omkar.makarand-waikar@capgemini.com>
2023-10-03 13:19:48 +0530

    Added combined reducers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { createStore } from 'redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import movies from './reducers';
+import rootReducer from './reducers';
 
 
-const store = createStore(movies);
+const store = createStore(rootReducer);
 // console.log('store', store);
 // console.log('Brfore State', store.getState());
 
